d513211fa819c59cc8c8b4dd8b4ad694fdc94dc8
MH <sayhi2moody@gmail.com>
2012-06-23 00:18:56 -0700

    merged in/ out one step fn's

--- main.c
+++ main.c
@@ -33,11 +33,11 @@ unsigned const char interleave_seq[] = {BIT1, BIT1|BIT2, BIT2, BIT2|BIT3, BIT3, 
 #define MAX_X				(unsigned long)(215900)		// Standard letter size, in micro meters
 #define MAX_Y				(unsigned long)(279400)
 #define MAX_LEN				(unsigned long)(353096)		// Length of diagonal, pre-calculated
-#define REEL_DIA			(unsigned long)(10000)		// Diameter of the spool that each motor controls
+#define REEL_DIA			(unsigned long)(1000)		// Diameter of the spool that each motor controls
 #define LEN_REELED_PER_REV	(unsigned long)(31415)		// pi * d
 #define LEN_REELED_PER_STEP	(unsigned long)(654)		// (pi * d)/48
 
-#define MAX_TIMER_INDX		(1)
+#define MAX_TIMER_INDX		(7)
 #define TIMER_ONE_SHOT		(1)
 
 /*
@@ -51,17 +51,18 @@ typedef enum{
 	PORT_P2
 }OP_PORT;
 
-typedef struct{
-	int				curr_seq; 	// The current bit pattern to drive the motor
-	unsigned char	*mode;		// one phase, two phase or interleaved
-	OP_PORT			port;		// Output port that the motor is wired to
-	unsigned long 	len;		// The length reeled in or out so far
-} MOTOR_DESC;
-
 typedef enum{
 	REEL_IN 	=  1,
 	REEL_OUT 	= -1
 } REEL_DIR;
+
+typedef struct{
+	int				curr_seq; 	// The current bit pattern to drive the motor
+	unsigned char	*mode;		// one phase, two phase or interleaved
+	OP_PORT			port;		// Output port that the motor is wired to
+	REEL_DIR		d;			// Current reel in/ out direction
+	unsigned long 	len;		// The length reeled in or out so far
+} MOTOR_DESC;
 
 /*
 typedef enum{
@@ -95,35 +96,40 @@ int register_timer(
 		int 				dur,	// duration in multiples of 1ms
 		int					type);	// # times to fire
 
-inline void one_step_in(
-		void 				*motor_desc);
+inline void one_step(
+		void 				*motor_desc);	// Callback fn from timer to step motor 1
 
-inline void one_step_out(
-		void 				*motor_desc);
-
-inline void stop_motor(
+void stop_motor(
 		MOTOR_DESC			*m);
 
-inline void reel_in_out(
+void reel_in_out(
 		MOTOR_DESC 			*m,		// Which motor to act on
-		unsigned long 		len, 	// Length to reel in/ out
-		REEL_DIR 			dir);	// in or out
+		unsigned long 		len); 	// Length to reel in/ out
 
 void goto_xy(
 		MOTOR_DESC 			*m1,	// X motor
 		MOTOR_DESC 			*m2, 	// Y motor
-		unsigned long 		x, 		// x & Y coordinates
+		unsigned long 		x, 		// Where to go- x & Y coordinates
 		unsigned long 		y);
 
-unsigned char *malloc();
+/*unsigned char *malloc();
 
 int free(unsigned char *m);
+*/
 
 // Global variables
 MOTOR_DESC	m1, m2;
 // MEM_POOL 	g_mem_pool[4];
 
-TIMER_CONTEXT timer_context[MAX_TIMER_INDX+1] = {{NULL, NULL, 0, 0, TIMER_ONE_SHOT}, {NULL, NULL, 0, 0, TIMER_ONE_SHOT}};	// Service max 2 requests for now- one for each motor
+TIMER_CONTEXT timer_context[MAX_TIMER_INDX+1] = {
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT},
+		{NULL, NULL, 0, 0, TIMER_ONE_SHOT}};	// Service max 8 requests for now- one for each motor
 
 void main(void)
 {
@@ -137,25 +143,29 @@ void main(void)
 	m1.mode = (unsigned char *)one_phase_seq;
 	m1.port = PORT_P1;
 	m1.len = 0;
+	m1.d = REEL_IN;
 
 	m2.curr_seq = 0;
 	m2.mode = (unsigned char *)one_phase_seq;
 	m2.port = PORT_P2;
-	m2.len = MAX_Y;
-
-	P2OUT = BIT0+BIT1+BIT2;
+	m2.len = 0;
+	m2.d = REEL_IN;
 
 	// register_timer(test_timer, NULL, 1, 5);
-	goto_xy( &m1, &m2, 100000, 130000);
+	goto_xy( &m1, &m2, 0, MAX_Y);
+	goto_xy( &m1, &m2, MAX_X, MAX_Y);
+	goto_xy( &m1, &m2, MAX_X, 0);
+	goto_xy( &m1, &m2, 0, 0);
 
 	// Setup Timer A - Last step
 	TACCTL0 = CCIE;        			// CCR0 interrupt enabled
 	TACCR0 = 32768;
-	TACTL = TASSEL_2 + MC_2;        // SMCLK, contmode
+	TACTL = TASSEL_2 + MC_1;        // SMCLK, contmode
 	_BIS_SR(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
+
 }
 
-inline void one_step_in(void *p_v)
+inline void one_step(void *p_v)
 {
 	MOTOR_DESC	*p_m = p_v;
 
@@ -166,34 +176,16 @@ inline void one_step_in(void *p_v)
 			case PORT_P2: P2OUT = BIT0 + p_m->mode[p_m->curr_seq]; break;
 			default: break;
 		}
-		p_m->curr_seq--;
+		p_m->curr_seq += p_m->d;	// Evaluates to ++ or --
 		if( p_m->curr_seq < 0 ) p_m->curr_seq = MAX_SEQ_INDX;
-		p_m->len -= LEN_REELED_PER_STEP;
+		else if( p_m->curr_seq > MAX_SEQ_INDX ) p_m->curr_seq = 0;
+		p_m->len += p_m->d * LEN_REELED_PER_STEP;
 	}
 	if(p_m->len > MAX_LEN) // Since this is unsigned, it will wrap around -ve values
 		p_m->len= MAX_LEN;
 }
 
-inline void one_step_out(void *p_v)
-{
-	MOTOR_DESC	*p_m = p_v;
-
-	if(p_m->len <= MAX_LEN)		// Not already reeled out fully
-	{
-		switch(p_m->port) {
-			case PORT_P1: P1OUT = BIT0 + p_m->mode[p_m->curr_seq]; break;
-			case PORT_P2: P2OUT = BIT0 + p_m->mode[p_m->curr_seq]; break;
-			default: break;
-		}
-		p_m->curr_seq++;
-		if( p_m->curr_seq > MAX_SEQ_INDX ) p_m->curr_seq = 0;
-		p_m->len += LEN_REELED_PER_STEP;
-	}
-	if(p_m->len > MAX_LEN)
-		p_m->len= MAX_LEN;
-}
-
-inline void stop_motor(MOTOR_DESC *m)
+void stop_motor(MOTOR_DESC *m)
 {
 	switch(m->port) {
 		case PORT_P1: P1OUT = 0x0; break;
@@ -204,24 +196,35 @@ inline void stop_motor(MOTOR_DESC *m)
 
 void goto_xy(MOTOR_DESC *m1, MOTOR_DESC *m2, unsigned long x, unsigned long y)
 {
-	if(m1->len > x) reel_in_out(m1, (m1->len)-x, REEL_IN);
-	else if(m1->len < x) reel_in_out(m1, x-(m1->len), REEL_OUT);
+	if(m1->len > x)
+	{
+		m1->d = REEL_IN;
+		reel_in_out(m1, (m1->len)-x);
+	}
+	else if(m1->len < x)
+	{
+		m1->d = REEL_OUT;
+		reel_in_out(m1, x-(m1->len));
+	}
 
-	if(m2->len > y) reel_in_out(m2, (m2->len)-y, REEL_IN);
-	else if(m2->len < y) reel_in_out(m2, y-(m2->len), REEL_OUT);
+	if(m2->len > y)
+	{
+		m2->d = REEL_IN;
+		reel_in_out(m2, (m2->len)-y);
+	}
+	else if(m2->len < y)
+	{
+		m2->d = REEL_OUT;
+		reel_in_out(m2, y-(m2->len));
+	}
 }
 
-inline void reel_in_out(MOTOR_DESC *m, unsigned long len, REEL_DIR dir)
+void reel_in_out(MOTOR_DESC *m, unsigned long len)
 {
 	int			num_steps;
 	num_steps = (int)(len/LEN_REELED_PER_STEP);
 
-	switch(dir)
-	{
-		case REEL_IN: register_timer(one_step_in, m, STEP_INTERVAL, num_steps); break;
-		case REEL_OUT: register_timer(one_step_out, m, STEP_INTERVAL, num_steps); break;
-		default: break;
-	}
+	register_timer(one_step, m, STEP_INTERVAL, num_steps);
 }
 
 // Timer A0 interrupt service routine
