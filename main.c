421463a4dfb8d2acb8c32fd3284987204c2c79df
MH <sayhi2moody@gmail.com>
2012-06-25 23:31:55 -0700

    my_sqrt

--- main.c
+++ main.c
@@ -14,18 +14,19 @@
  * M1 enabled on P1.0 and M2 on P2.0
  * P1.1-P1.3 controls M1
  * P2.1-P2.3 controls M2
+ * When pen is at (0,0), M1 will be REEL_OUT to MAX_Y and M2 to MAX (along diagonal)
  */
 
 #include <msp430g2452.h>
 
+#define ABS(x)	((x)<0?(-x):(x))
+
 unsigned const char one_phase_seq[] = {BIT1, BIT2, BIT3, BIT4, BIT1, BIT2, BIT3, BIT4};	// Bits are lt. shifted 1 since first bit is enable bit which is set separately
 unsigned const char two_phase_seq[] = {BIT1|BIT2, BIT2|BIT3, BIT3|BIT4, BIT4|BIT1, BIT1|BIT2, BIT2|BIT3, BIT3|BIT4, BIT4|BIT1};
 unsigned const char interleave_seq[] = {BIT1, BIT1|BIT2, BIT2, BIT2|BIT3, BIT3, BIT3|BIT4, BIT4, BIT4|BIT1};
+#define MAX_SEQ_INDX		(7)		// size of _seq[] array -1
 
 #define NULL				((void *)0)
-#define MAX_SEQ_INDX		(7)		// size of _seq[] array -1
-#define CW 					(1)
-#define CCW 				(-1)
 #define STEPS_PER_TURN		(48)
 #define	MAX_RPM				(600)
 #define STEP_INTERVAL		(2)	// 2 ms. 600*48 steps/ min; 480 steps/ sec for an interval between steps of ~2ms
@@ -37,12 +38,12 @@ unsigned const char interleave_seq[] = {BIT1, BIT1|BIT2, BIT2, BIT2|BIT3, BIT3, 
  * # steps required to cover the 21.59cm in the X direction = 21.59/.654 = 330
  * # steps required to cover the 27.94cm in the Y direction = 27.94/.654 = 427
  * #steps in the diagonal = sqrt(x^2+y^2)/ 654 = 540
+ * Defining X & Y limits in # of steps allows working with smaller numbers
  */
 #define MAX_X_STEPS			(330)
 #define MAX_Y_STEPS			(427)
 #define MAX_STEPS			(540)
 
-#define DELAY_1MS			(0x3fff)
 /*
 #define MAX_TIMERS			(25)	// May be as many events pending as # of steps
 #define TIMER_ONE_SHOT		(1)
@@ -69,7 +70,7 @@ typedef struct{
 	unsigned char	*mode;		// one phase, two phase or interleaved
 	OP_PORT			port;		// Output port that the motor is wired to
 	REEL_DIR		d;			// Current reel in/ out direction
-	unsigned int	curr_step_pos;		// Current X/Y position, in # of steps from origin
+	unsigned int	curr_step_len;		// Current X/Y position, in # of steps
 } MOTOR_DESC;
 
 /*
@@ -101,7 +102,7 @@ int register_timer(
 		void 				*p_args);// Args to be passed back to the call back fn
 */
 void one_step(
-		void				*motor_desc);	// Callback fn from timer to step motor 1
+		MOTOR_DESC			*motor_desc);
 
 void stop_motor(
 		MOTOR_DESC			*m);
@@ -115,6 +116,9 @@ void lineto_xy(
 void my_delay(
 		int					delay);	// Delay in multiples of 10ms
 
+unsigned int my_sqrt(
+		unsigned long		x);		// My integer square root implementation
+
 /*unsigned char *malloc();
 
 int free(unsigned char *m);
@@ -122,6 +126,8 @@ int free(unsigned char *m);
 
 // Global variables
 MOTOR_DESC	m1, m2;
+unsigned int g_curr_x, g_curr_y;
+
 // MEM_POOL 	g_mem_pool[4];
 //debug
 int g_d_onestep, g_d_p1, g_d_p2, g_timer_full_err;
@@ -138,17 +144,21 @@ void main(void)
 	P1OUT = P2OUT = 0x0;	// All pulled low to start
 	P1DIR = P2DIR = 0xff;	// Initialize P1, P2 to all outputs
 
-	// Initialize motor descriptors- Assumes start position is (0, MAX_Y)
+	// Initialize motor descriptors- Assumes start position is at origin
+	// Since origin is lower left corner and motors are mounted on top left & right corners,
+	// the starting position of (m1, m2) is (MAX_Y_STEPS, MAX_STEPS {Along diagonal})
 	m1.curr_seq = 0;
 	m1.mode = (unsigned char *)one_phase_seq;
 	m1.port = PORT_P1;
-	m1.curr_step_pos = 0;
+	m1.curr_step_len = MAX_Y_STEPS;
+	g_curr_x = 0;
 	m1.d = REEL_OUT;
 
 	m2.curr_seq = 0;
 	m2.mode = (unsigned char *)one_phase_seq;
 	m2.port = PORT_P2;
-	m2.curr_step_pos = 0;
+	m2.curr_step_len = MAX_STEPS;
+	g_curr_y = 0;
 	m2.d = REEL_OUT;
 
 
@@ -170,19 +180,20 @@ void main(void)
 	//_BIS_SR(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
 	__enable_interrupt();			// Enable interrupts globally
 
-	lineto_xy( &m1, &m2, 20, 100);
+	lineto_xy( &m1, &m2, 0, 0);
 	my_delay(1000);
-	lineto_xy( &m1, &m2, 0, 10);
+	lineto_xy( &m1, &m2, MAX_X_STEPS, 0);
+
+	lineto_xy( &m1, &m2, MAX_X_STEPS, MAX_Y_STEPS);
+
+	lineto_xy( &m1, &m2, 0, MAX_Y_STEPS);
 }
 
-void one_step(void *p_v)
+void one_step(MOTOR_DESC *p_m)
 {
-	MOTOR_DESC	*p_m;
-
-	p_m = (MOTOR_DESC *)p_v;
 	g_d_onestep ++;
 
-	if(p_m->curr_step_pos < MAX_STEPS)		// Not already reeled out fully
+	if(p_m->curr_step_len < MAX_STEPS)		// Not already reeled out fully
 	{
 		switch(p_m->port) {
 			case PORT_P1: P1OUT = BIT0 + p_m->mode[p_m->curr_seq]; g_d_p1++; break;
@@ -208,66 +219,53 @@ void stop_motor(MOTOR_DESC *m)
 
 void lineto_xy(MOTOR_DESC *m1, MOTOR_DESC *m2, unsigned int x2, unsigned int y2)
 {
-	int xinc, yinc, xerr2, yerr2, dx, dy, dx2, dy2;
-	unsigned int *x, *y;
+	unsigned int x1, y1, lx, ly;
+	int i,j,k,l;
+	long tmp;
 
-	x = &(m1->curr_step_pos);
-	y = &(m2->curr_step_pos);
+	x1 = g_curr_x;
+	y1 = g_curr_y;
 
-	xinc = yinc = 1;;
+	i = x1+x2; j = x1-x2;
+	k = y1+y2; l = y1-y2;
 
-	dx = x2 - *x;
-	m1->d = m2-> d = REEL_OUT;
-	if( dx < 0 )
+	tmp = i*j+k*l-2*MAX_Y_STEPS*l;
+	lx = (unsigned int)my_sqrt(ABS(tmp));
+	ly = (unsigned int)my_sqrt(ABS(tmp-2*MAX_X_STEPS*j));
+
+
+	if(j < 0)	// Reel out since the destination point is lower than the starting X coord
 	{
-		xinc = -1;
+		m1->d = REEL_OUT;
+		m1->curr_step_len += lx;
+	}
+	else
+	{
 		m1->d = REEL_IN;
-		dx = -dx;
+		m1->curr_step_len -= lx;
 	}
-	dx2 = dx * 2;
-
-	dy = y2 - *y;
-	if( dy < 0 )
+	if(l < 0)	// Reel out since the destination point is lower than the starting Y coord
 	{
-		yinc = -1;
+		m2->d = REEL_OUT;
+		m2->curr_step_len += ly;
+	}
+	else
+	{
 		m2->d = REEL_IN;
-		dy = -dy;
+		m2->curr_step_len -= ly;
 	}
-	dy2 = dy * 2;
-
-	if( (0 != dx) | (0 != dy) )
+	while(lx>0)
 	{
-		if( dy <= dx )		// Slope <= 1
+		one_step(m1);
+		lx--;
+		if(ly >0)
 		{
-			xerr2 = 0;
-			do{
-				*x += xinc;
-				xerr2 += dy2;
-				one_step(m1);
-				if( xerr2 > dx )
-				{
-					*y += yinc;
-					one_step(m2);
-					xerr2 -= dx2;
-				}
-			} while(*x != x2);
-		}
-		else
-		{
-			yerr2 = 0;
-			do {
-				*y += yinc;
-				one_step(m2);
-				yerr2 += dx2;
-				if( yerr2 > dy )
-				{
-					*x += xinc;
-					one_step(m1);
-					xerr2 -= dy2;
-				}
-			} while(*y != y2);
+			one_step(m2);
+			ly--;
 		}
 	}
+
+	g_curr_x = x2; g_curr_y = y2;
 }
 
 void my_delay(int d)
@@ -355,3 +353,31 @@ int free(unsigned char *m)
 	return i;
 }
 */
+
+/*
+ * Assumes int is 16 bits and char is 8 bits
+ */
+unsigned int my_sqrt(unsigned long x) -- Fix this
+{
+	unsigned int j = 0x8000, l = 0x8000, m;
+	unsigned long k;
+
+	do {
+		k = j*j;
+		l >>=1;
+		if( k == x)		// Found a perfect sqrt
+			break;
+		else if(k > x)	// Over shot. Need to undo the previous bit op
+		{
+			m = ~(l<<1); // Mask out all the bits except for the last op
+			j &= m;
+			j +=l;
+		}
+		else
+		{
+			j += l;
+		}
+	}while(l>1);
+
+	return j;
+}
